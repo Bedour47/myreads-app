bbb06dc0e9290626cc66f8afd9540345db4dba9b
Bedour47 <bedour.ir@gmail.com>
2019-09-20 20:30:01 +0300

    Updates

---
+++ precache-manifest.f63c4cf6192c251b89c482fc52422cdf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b783d6d0c66f55628a0",
+    "url": "/react-myreads/static/css/main.c4f259cc.chunk.css"
+  },
+  {
+    "revision": "7b783d6d0c66f55628a0",
+    "url": "/react-myreads/static/js/main.7b783d6d.chunk.js"
+  },
+  {
+    "revision": "a756d391277592e45a35",
+    "url": "/react-myreads/static/js/1.a756d391.chunk.js"
+  },
+  {
+    "revision": "9eb48c254ff4f98c2a77",
+    "url": "/react-myreads/static/js/runtime~main.9eb48c25.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/react-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/react-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/react-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "96f8eb3c85b26d21a1ec807dafb135b8",
+    "url": "/react-myreads/index.html"
+  }
+];
